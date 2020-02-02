bd3804c30655c21684eb3a6800e25856e0049643
vcurus <vcurus@Yndex.ru>
2020-02-02 13:59:31 +1000

    урок какойто

--- main.py
+++ main.py
@@ -23,43 +23,51 @@ level = [
     '-                                                                  --                                                                                      -',
     '------------------------------------------------------------------------------------------------------------------------------------------------------------']
 
-BRICK_WIDTH, BRICK_HEIGHT = 30, 30
-BRICK_COLOR = (0, 255, 0)
-BRICK_COLOR_2 = (255, 255, 0)
 WIN_WIDTH, WIN_HEIGHT = 780, 630
 BG_COLOR = (192, 192, 192)
-RED = (255, 0, 0)
-BG_COLOR_2 = (192, 192, 192), (True, RED, None)
+BRICK_WIDTH = BRICK_HEIGHT = 30
+BRICK_COLOR = (0, 128, 0)
+BRICK_COLOR_2 = (255, 128, 0)
 FPS = 60
 clock = pygame.time.Clock()
-x1, y1 = WIN_WIDTH // 2, WIN_HEIGHT // 2
 PLAYER_SIZE = 40
 BG_SPEED = 3
 dx = 0
 PLAYER_SPEED = 3
 penalty = 0.0
 BTN_W, BTN_H = 220, 60
+GOLD = (255, 215, 0)
+BLUE = (0, 0, 255, 255)
+RED = (255, 0, 0)
+WHITE = (255, 255, 255)
 
 pygame.init()
-pygame.display.set_caption("первая игра")
+pygame.display.set_caption('первая игра')
 screen = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
-player = pygame.Surface((BRICK_WIDTH, BRICK_HEIGHT))
 
-player = pygame.Surface((PLAYER_SIZE, PLAYER_SIZE))
-player.set_colorkey((0, 0, 0))
-pygame.draw.circle(player, (0, 0, 250), (PLAYER_SIZE // 2, PLAYER_SIZE // 2), PLAYER_SIZE // 2)
-pygame.draw.circle(player, (255, 215, 0), (12, 15), 4)
-pygame.draw.circle(player, (255, 215, 0), (28, 15), 4)
-pygame.draw.arc(player, (255, 215, 0), (0, 12, 24, 20), 3.6, 6.0, 3)
+player = pygame.Surface((PLAYER_SIZE, PLAYER_SIZE), pygame.SRCALPHA)
+# player.set_colorkey((0, 0, 0))
+
+
+def face(color):
+    pygame.draw.circle(player, color, (PLAYER_SIZE // 2, PLAYER_SIZE // 2), PLAYER_SIZE // 2)
+    pygame.draw.circle(player, GOLD, (12, 15), 4)
+    pygame.draw.circle(player, GOLD, (28, 15), 4)
+    pygame.draw.arc(player, GOLD, (8, 12, 24, 20), 3.6, 6.0, 3)
+
+
 player_rect = player.get_rect(center=(WIN_WIDTH // 2, WIN_HEIGHT // 2))
 
 text = pygame.font.SysFont('Arial', 22, True, False)
-text_xy = ((WIN_WIDTH - text.size(f'Штрафных очкв {round(penalty, 1)}')[0]) // 2, 30)
+text_xу = ((WIN_WIDTH - text.size(f'Штрафных очков: {round(penalty, 1)}')[0]) // 2, 30)
 
 btn = pygame.Surface((BTN_W, BTN_H))
-text1 = 'Играть снова!'
+btn.fill(BLUE)
+text1 = 'ИГРАТЬ СНОВА ?'
 text1_xy = text.size(text1)
 
+color = BLUE
+face(color)
 run = True
 while run:
     for e in pygame.event.get():
@@ -68,43 +76,55 @@ while run:
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_RIGHT]:
-        player_rect.x += PLAYER_SPEED 
-
+        player_rect.x += PLAYER_SPEED
     if keys[pygame.K_LEFT]:
-        player_rect.x -= PLAYER_SPEED 
-
+        player_rect.x -= PLAYER_SPEED
     if keys[pygame.K_UP]:
         player_rect.y -= PLAYER_SPEED
-
     if keys[pygame.K_DOWN]:
         player_rect.y += PLAYER_SPEED
 
     screen.fill(BG_COLOR)
-    dx -= BG_SPEED
-    if dx > WIN_WIDTH * 4:
-       dx -= BG_SPEED 
+    if color == RED:
+        color = BLUE
+        face(color)
+
+    if dx > -WIN_WIDTH * 4:
+        dx -= BG_SPEED
     else:
-       if player_rect.x < WIN_WIDTH - PLAYER_SIZE:
-           player_rect.x += PLAYER_SPEED
+        if player_rect.x < WIN_WIDTH - PLAYER_SIZE:
+            player_rect.x += PLAYER_SPEED
 
     x = dx
     y = 0
     for row in level:
         for col in row:
-            if col == "-":
-                # screen.blit(brick, (x, y))
+            if col == '-':
                 brick = pygame.draw.rect(screen, BRICK_COLOR, [x, y, BRICK_WIDTH, BRICK_HEIGHT])
-                pygame.draw.rect(screen, (BRICK_COLOR_2), [x, y, BRICK_WIDTH, BRICK_HEIGHT], 2)
+                pygame.draw.rect(screen, BRICK_COLOR_2, [x, y, BRICK_WIDTH, BRICK_HEIGHT], 2)
                 if brick.colliderect(player_rect):
+                    if color == BLUE:
+                        color = RED
+                        face(color)
                     penalty += 0.1
             x += BRICK_WIDTH
         y += BRICK_HEIGHT
         x = dx
 
-    screen.blit(player, player_rect)
-    pygame.display.set_caption(F' FPS: {round(clock.get_fps(), 2)}')
-    screen.blit(
-        text.render(f'Штрафных очкв {round(penalty, 1)}', True, RED, None), text_xy)
+    if player_rect.x < WIN_WIDTH - PLAYER_SIZE:
+        screen.blit(player, player_rect)
+        screen.blit(
+            text.render(f'Штрафных очков: {round(penalty, 1)}', True, RED, None), text_xу)
+    else:
+        screen.blit(btn, ((WIN_WIDTH - BTN_W) // 2, WIN_HEIGHT // 2))
+        btn.blit(
+            text.render(text1, True, WHITE, None),
+            ((BTN_W - text1_xy[0]) // 2, (BTN_H - text1_xy[1]) // 2))
+        screen.blit(
+            text.render(f'Штрафных очков: {round(penalty, 1)}', True, RED, None),
+            ((WIN_WIDTH - text.size(f'Штрафных очков: {round(penalty, 1)}')[0]) // 2,
+             WIN_HEIGHT // 2 - BTN_H))
 
+    pygame.display.set_caption(f' FPS: {round(clock.get_fps(), 2)}')
     pygame.display.update()
     clock.tick(FPS)
